84932881dcf132d5d1e988fce80a31925f2ef79f
sanmiguel.cacar <juancarlossanmiguelcarmona13@gmail.com>
2025-04-24 20:13:09 +0200

    Corrije error 2

--- src/GestorEstudiantes.java
+++ src/GestorEstudiantes.java
@@ -10,7 +10,8 @@ public class GestorEstudiantes {
         for (int i = 0; i < estudiante.getNotas().length; i++) { // Error: índice fuera de rango
             suma += estudiante.getNotas()[i];
         }
-        return suma / estudiante.getNotas().length; // Error si el array está vacío
+        if (suma!=0)  return suma / estudiante.getNotas().length; // Error si el array está vacío
+        else return 0;
     }
 
     // Encuentra al estudiante con la mejor nota media
